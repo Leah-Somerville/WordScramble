1875c3b7cc7c1b16bd7e4dafc35980004b7b404f
Leah Somerville <leah.somerville4@gmail.com>
2024-02-23 09:44:35 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Leah Somerville on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
